891c4970c8324cecc6cdd9bfa72ee36f9e460ceb
Minh Hang Hoang Thi <minhhanghoangthi@Minh-Hangs-Mac.local>
2011-08-20 18:42:26 +0700

    ok

--- MobileMovie/js/searchresultpage.js
+++ MobileMovie/js/searchresultpage.js
@@ -2,59 +2,58 @@
  * SearchResultPage
  */
 Page.extend("SearchResultPage",
-// static methods 
-{},
+// static methods
+{
+},
 // prototype methods
 {
-	
-	movieListItemTemplate: null,
-	resultList: null,
-	scroller: null,
-	
-	init : function(id){	
+
+	movieListItemTemplate : null,
+	resultList : null,
+	scroller : null,
+
+	init : function(id) {
 		this._super(id);
-		this.movieListItemTemplate = this.el.find("#movieListItem");		
+		this.movieListItemTemplate = this.el.find("#movieListItem");
 		this.resultList = this.el.find("#resultList");
 		//this.scroller = this.el.find("#wrapper");
 		//this.scroller.touchScroll();
-		
+
 	},
 	/*
 	 * call when page opens
 	 */
-	onPageShow: function(e, ui){
+	onPageShow : function(e, ui) {
 		this._super(e, ui);
 		var me = this;
-		this.resultList.listview();	
+		this.resultList.listview();
 		//fill list
-		this.resultList.empty();		  
+		this.resultList.empty();
 		this.movieListItemTemplate.tmpl(Page.exchangeData.movieSearchResult.contentData).appendTo(this.resultList);
-		
-		this.resultList.find("a").live("vclick", function(e){me.listItemButtonClicked(e);});
+
+		this.resultList.find("a").live("vclick", function(e) {
+			me.listItemButtonClicked(e);
+		});
 		//refresh list
 		this.resultList.listview("refresh");
-		setTimeout(function () {
+		setTimeout(function() {
 			myScroll.refresh();
-		}, 0);		
+		}, 0);
 	},
-	
-	listItemButtonClicked: function(e){
-		  e.stopPropagation();
-		  var btn = $(e.target);
-		  var id = btn.attr("data-messageId");
-		  
-		  //Page.exchangeData.resSelect = {movID : id};
-		  
-		  $.mobile.pageLoading();
-		  AjaxService.getResList(id, function(d){	  
-			  
-			  Page.exchangeData.resListResult = d;
-			  
-			  $.mobile.pageLoading(true);
-			  $.mobile.changePage("#ResSelectPage");
-		  });
-		  
-		 
+	listItemButtonClicked : function(e) {
+		e.stopPropagation();
+		var btn = $(e.target);
+		var id = btn.attr("data-messageId");
+
+		//Page.exchangeData.resSelect = {movID : id};
+
+		$.mobile.pageLoading();
+		AjaxService.getResList(id, function(d) {
+
+			Page.exchangeData.resListResult = d;
+
+			$.mobile.pageLoading(true);
+			$.mobile.changePage("#ResSelectPage");
+		});
 	}
-  
 });
