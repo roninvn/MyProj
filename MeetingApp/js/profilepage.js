ff01772acccb508cd8bf9cf5e4582c7de0d85265
Minh Hang Hoang Thi <minhhanghoangthi@Minh-Hangs-Mac.local>
2011-08-21 21:59:52 +0700

    abc

---
+++ MeetingApp/js/profilepage.js
@@ -0,0 +1,13 @@
+/*
+ * ProfilePage
+ */
+Page.extend("ProfilePage",
+// static methods 
+{},
+// prototype methods
+{
+  // called when a new instance is created
+  init : function(id){
+	  this._super(id);
+  }
+});
---
+++ MeetingApp/js/signup.js
@@ -0,0 +1,13 @@
+/*
+ * Signup Page
+ */
+Page.extend("SignupPage",
+// static methods 
+{},
+// prototype methods
+{
+  // called when a new instance is created
+  init : function(id){
+	  this._super(id);
+  }
+});
