b91720dc507e8098ba508fb42e3c1c4aab54509c
Nam <Nam@Nam-PC>
2011-08-26 19:53:37 +0700

    Change-Id: I140da539f5e00f872724ca02d8d0eff922c1b884 Signed-off-by: Nam <Nam@Nam-PC>

--- mowbi/address.php
+++
@@ -1,8 +0,0 @@
-<link href="../3/css/onepage.css" rel="stylesheet" type="text/css">
-
-      <div class="address-box"> 
-        <address> 
-        <strong>{$op.PageTitle}</strong>
-        {$op.Address|nl2br} {if $op.City neq ''}{$op.City},{else} {if $directory.City neq ''} {$directory.City} {/if} {/if} {$op.State} {$op.ZIP}
-        </address> 
-      </div>
