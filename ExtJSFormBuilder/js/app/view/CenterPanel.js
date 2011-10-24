e66ea79f3fcddc808fc51456fb3e5f76e8ef66ef
Nam Hoang <Nam Hoang@NamHoang-Win7>
2011-10-24 10:09:11 +0700

    Change-Id: I83746a8b80e24fc2994136da1e96442e8306378d Signed-off-by: Nam Hoang <Nam Hoang@NamHoang-Win7>

--- ExtJSFormBuilder/js/app/view/CenterPanel.js
+++ ExtJSFormBuilder/js/app/view/CenterPanel.js
@@ -1,77 +1,81 @@
 /**
- * This file will create the Center Panel and dock it to the viewport of the system.
+ * This file will create the Center Panel and dock it to the viewport of the
+ * system.
+ * 
  * @author Nam Hoang
  */
 Ext.define('FB.view.CenterPanel', {
-    extend: 'Ext.panel.Panel',
-    activeItem: 0,
-    margins: '5 5 5 5',
-	region: 'center', // a center region is ALWAYS required for border layout
-    deferredRender: false,   
-    alias: 'widget.CenterPanel',
-    layout: 'absolute',
-    
-    requires:['FB.view.DesignControl'],
-	 /**
-     * This function is used to init it own component and call parent arguments
-     * 
-     * @author Nam Hoang
-     * 
-     * @return void
-     */
-    initComponent: function() {		
+	extend : 'Ext.panel.Panel',
+	activeItem : 0,
+	margins : '5 5 5 5',
+	region : 'center', // a center region is ALWAYS required for border layout
+	deferredRender : false,
+	alias : 'widget.CenterPanel',
+	layout : 'absolute',
+
+	requires : [ 'FB.view.DesignControl' ],
+	/**
+	 * This function is used to init it own component and call parent arguments
+	 * 
+	 * @author Nam Hoang
+	 * 
+	 * @return void
+	 */
+	initComponent : function() {
 		this.callParent(arguments);
-		
+
 		var nav = new Ext.util.KeyNav(Ext.get(document), {
-				    "del" : function(e){
-				        if(FB.view.DesignControl.selectedControl){
-				        	this.remove(FB.view.DesignControl.selectedControl.ctr, true);
-				        	FB.view.DesignControl.selectedControl.ctr = null;
-				        }
-				    },
-				    scope : this
+			"del" : function(e) {
+				if (FB.view.DesignControl.selectedControl) {
+					this
+							.remove(FB.view.DesignControl.selectedControl.ctr,
+									true);
+					FB.view.DesignControl.selectedControl.ctr = null;
+				}
+			},
+			scope : this
 		});
-    },
-    
-    listeners: {
-        render: function(grid){
-        	
-        	grid.dropZone = Ext.create('Ext.dd.DropZone', grid.el, {
+	},
 
-                getTargetFromEvent: function(e) {                	
-                    return grid.el;
-                },
-                
+	listeners : {
+		render : function(grid) {
 
-                onNodeOver : function(target, dd, e, data){                	
-                    return Ext.dd.DropZone.prototype.dropAllowed;
-                },
+			grid.dropZone = Ext.create('Ext.dd.DropZone', grid.el, {
 
-                onNodeDrop : function(target, dd, e, data){
+				getTargetFromEvent : function(e) {
+					return grid.el;
+				},
 
-                	var x = e.getX()-grid.el.getX(),
-                		y=e.getY()-grid.el.getY();
-                	
-                	if(data.isDesign){
-                		data.control.setPosition(x,y);
-                		
-                		data.control.designControl.oCfg.x = x;
-                		data.control.designControl.oCfg.y = y;
-                	}
-                	else{
-                	
-	                	var cdt = Ext.clone(data.controlData);
-	                	cdt.x = x;
-	                	cdt.y = y;
-	                	
-	                	var c = Ext.create('FB.view.DesignControl',{cdt : cdt});
-	                	                	
-	                	grid.add(c.ctr);
-                	}
-                    return true;
-                }
-            });
-        }
-    }
-    
+				onNodeOver : function(target, dd, e, data) {
+					return Ext.dd.DropZone.prototype.dropAllowed;
+				},
+
+				onNodeDrop : function(target, dd, e, data) {
+
+					var x = e.getX() - grid.el.getX(), y = e.getY()
+							- grid.el.getY();
+
+					if (data.isDesign) {
+						data.control.setPosition(x, y);
+
+						data.control.designControl.oCfg.x = x;
+						data.control.designControl.oCfg.y = y;
+					} else {
+
+						var cdt = Ext.clone(data.controlData);
+						cdt.x = x;
+						cdt.y = y;
+
+						var c = Ext.create('FB.view.DesignControl', {
+							cdt : cdt
+						});
+
+						grid.add(c.ctr);
+					}
+					return true;
+				}
+			});
+		}
+	}
+
 });
