afa24de90612183e766dd329f9e7a5d07db4af05
Nam <Nam@Nam-PC>
2011-12-13 06:23:42 +0700

    Change-Id: I4aed97c8aaa999b75fefbfbef353608d558f131d Signed-off-by: Nam <Nam@Nam-PC>

--- TextAdv/js/content/001.js
+++ TextAdv/js/content/001.js
@@ -1,6 +1,6 @@
 pages[1] = {
 		links: [{text: "Park", page: 2}, {text: "Cinema", page: 3}],
-		content: "It is day one, decide where you would like to go:",
+		content: "It is day one, decide where you would like to go: Here is some text to make the page longer and test the scroll.  This uses an image, a red background, and the cornerImgCls of imgman1.  Select park to see the same sort of text but with no cornerImgCls, so the scroll should be gone on this too.  Hope this works, fingers crossed.  I think I'll change the background to blue for the next page",
 		bgColor: "red",
 		cornerImg: "img/man.png",
 		cornerImgCls: 'imgman1'
--- TextAdv/js/content/002.js
+++ TextAdv/js/content/002.js
@@ -1,6 +1,6 @@
 pages[2] = {
 		links: [{text: "Feed Birds", page: 4}, {text: "Get an ice-cream", page: 5}],
-		content: "You go to the park.  Would you like to feed the birds, or get an ice-cream?",
+		content: "You go to the park.  Would you like to feed the birds, or get an ice-cream? This should be a test of no ImgManCls, just to see if the scroll for this goes as well.  the background colour should be blue, and the same image used.  One more line should make this long enough, and I can check that both scroll bars are gone.  Now select Birds which uses a different imgMan",
 		bgColor: "blue",
 		cornerImg: "img/man.png"
 };
